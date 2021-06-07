c5437f0b404cc8a374478b6edc696b9277c67a93
euler5926 <euler5926@gmail.com>
2021-06-07 10:05:35 +1000

    dsdsd

--- src/index.js
+++ src/index.js
@@ -202,19 +202,19 @@ class Home extends React.Component {
         </form>
         <form  onSubmit={this.handleSubmit}>
           <h4>Add Food</h4>
-        <TextField type="text" name="foodname" variant="filled"
+        <TextField type="text" name="foodname" variant="filled"  pattern="[A-Za-z]" maxlength="30" 
         color="primary" label="foodname"
         style={{ margin: 10 ,display: 'inline-block' }}  id="foodname"  required></TextField>
 
-        <TextField type="number" name="price" variant="filled"
+        <TextField type="number" name="price" variant="filled" min="0" max="50"
         color="primary" label="price"
         style={{ margin: 10 ,display: 'inline-block' }} id="price"  required></TextField>
    
-        <TextField type="text" name="description" variant="filled"
+        <TextField type="text" name="description" variant="filled" 
         color="primary" label="description"
         style={{ margin: 10 ,display: 'inline-block' }} id="description"  required></TextField>
        
-        <TextField type="text" name="options"variant="filled"
+        <TextField type="text" name="options"variant="filled" pattern="[A-Za-z]" maxlength="20" 
         color="primary" label="options"
         style={{ margin: 10 ,display: 'inline-block' }}  id="options" required></TextField>
      
@@ -229,23 +229,23 @@ class Home extends React.Component {
        <form onSubmit={this.handleupdate}>
          <h4>Update food</h4>
            
-            <TextField type="number" name="F_ID" variant="filled"
+            <TextField type="number" name="F_ID" variant="filled" min="0" 
         color="primary" label="FoodID"
         style={{ margin: 10 ,display: 'inline-block' }} id="F_ID2"   required></TextField>
 
-            <TextField type="text" name="foodname" variant="filled"
+            <TextField type="text" name="foodname" variant="filled" pattern="[A-Za-z]" maxlength="30" 
         color="primary" label="foodname"
         style={{ margin: 10 ,display: 'inline-block' }} id="foodname2"  required></TextField>
       
-            <TextField type="number" name="price" variant="filled"
+            <TextField type="number" name="price" variant="filled" min="0" max="50" 
         color="primary" label="price"
         style={{ margin: 10 ,display: 'inline-block' }} id="price2"  required></TextField>
          
-            <TextField type="text" name="description" variant="filled"
+            <TextField type="text" name="description" variant="filled" 
         color="primary" label="description"
         style={{ margin: 10 ,display: 'inline-block' }} id="description2"  required></TextField>
             <TextField type="text" name="options" variant="filled"
-        color="primary" label="options"
+        color="primary" label="options" pattern="[A-Za-z]" maxlength="20" 
         style={{ margin: 10 ,display: 'inline-block' }} id="options2"  required></TextField>
       
             <TextField type="text" name="image" variant="filled"
@@ -332,7 +332,7 @@ class Login extends React.Component {
       <Form onSubmit={this.handleSubmit}>
           <div className="form-group">
               <label htmlFor="username">username</label>
-              <Field name="username" id="username" type="text" className={'form-control' + (errors.username && touched.username ? ' is-invalid' : '')} />
+              <Field name="username" id="username" pattern="[A-Za-z]"  type="text" className={'form-control' + (errors.username && touched.username ? ' is-invalid' : '')} />
               <ErrorMessage name="username" component="div" className="invalid-feedback" />
           </div>
           <div className="form-group">
@@ -408,18 +408,18 @@ class Sign extends React.Component {
          <h1>Sign Up</h1>
          <form  onSubmit={this.handleSubmit}>
              <TextField type="text" name="username" onChange={this.onChange.bind(this)} value={this.state.value} id="regusername" variant="filled"
-        color="primary"   label="username"
+        color="primary"   label="username" pattern="[A-Za-z]" maxlength="30" 
         style={{ margin: 10 ,display: 'inline-block' }} required></TextField>
             
-              <TextField type="email" name="email"  id="regemail"   variant="filled"
+              <TextField type="email" name="email"  id="regemail"   variant="filled" 
         color="primary"  label="email"
         style={{ margin: 10 ,display: 'inline-block' }}required></TextField>
  
-              <TextField type="text" name="phone"  id="regphone"  variant="filled"
+              <TextField type="text" name="phone"  id="regphone"  variant="filled" min="4000000000" max="4999999999"
         color="primary" label="phone"
         style={{ margin: 10 ,display: 'inline-block' }} required></TextField>
             
-              <TextField type="number" name="postcode"  id="regpostcode" variant="filled"
+              <TextField type="number" name="postcode"  id="regpostcode" variant="filled" min="0" max="9999"
         color="primary" label="postcode"
         style={{ margin: 10 ,display: 'inline-block' }} required></TextField>
              
@@ -492,13 +492,13 @@ class Setting extends React.Component {
       <form onSubmit={this.handleSubmit}>
               <TextField type="hidden" name="currentusername"  id="currentusername" required hidden></TextField>
               <h4> username</h4>
-              <TextField type="text" name="username"  id="upusername"  onChange={this.onChange.bind(this)} value={this.state.value} required></TextField>
+              <TextField type="text" name="username"  id="upusername" pattern="[A-Za-z]" maxlength="30"  onChange={this.onChange.bind(this)} value={this.state.value} required></TextField>
               <h4> email</h4>
               <TextField type="email" name="email"  id="upemail" required></TextField>
               <h4> phone</h4>
-              <TextField type="number" name="phone"  id="upphone"  required></TextField>
+              <TextField type="number" name="phone"  id="upphone" min="4000000000" max="4999999999" required></TextField>
               <h4> postcode</h4>
-              <TextField type="number" name="postcode"  id="uppostcode" required></TextField>
+              <TextField type="number" name="postcode"  id="uppostcode" min="0" max="9999" required></TextField>
               <h4> password</h4>
               <TextField type="password" name="password" placeholder="password" id="uppassword" required></TextField>
               <h4>re-password</h4>
@@ -650,7 +650,7 @@ class User extends React.Component {
         </form>
         <form  onSubmit={this.handleSubmit}>
         <h4> Add User</h4>
-             <TextField type="text" name="username" onChange={this.onChange.bind(this)} value={this.state.value} id="regusername" 
+             <TextField type="text" name="username" pattern="[A-Za-z]" maxlength="30"  onChange={this.onChange.bind(this)} value={this.state.value} id="regusername" 
              variant="filled"
              color="primary"   label="username"
              style={{ margin: 10 ,display: 'inline-block' }} required></TextField>
@@ -659,11 +659,11 @@ class User extends React.Component {
         color="primary"   label="email"
         style={{ margin: 10 ,display: 'inline-block' }} required></TextField>
            
-              <TextField type="text" name="phone"  id="regphone" variant="filled"
+              <TextField type="text" name="phone"  id="regphone" min="4000000000" max="4999999999"variant="filled"
         color="primary"   label="phone"
         style={{ margin: 10 ,display: 'inline-block' }} required></TextField>
             
-              <TextField type="number" name="postcode"  id="regpostcode"  variant="filled"
+              <TextField type="number" name="postcode"  id="regpostcode" min="0" max="9999" variant="filled"
         color="primary"   label="postcode"
         style={{ margin: 10 ,display: 'inline-block' }} required></TextField>
             
@@ -671,7 +671,7 @@ class User extends React.Component {
         color="primary"   label="password"
         style={{ margin: 10 ,display: 'inline-block' }} required></TextField>
         
-              <TextField type="usertype" name="usertype"  id="usertype" variant="filled"
+              <TextField type="usertype" name="usertype"  id="usertype" variant="filled" pattern="[A-Za-z]" maxlength="30" 
         color="primary"   label="usertype"
         style={{ margin: 10 ,display: 'inline-block' }} required></TextField> 
         <Button type="submit" variant="contained" color="primary">Add User</Button>
@@ -679,10 +679,10 @@ class User extends React.Component {
        <form onSubmit={this.handleupdate}>
        <h4> CustomerID</h4>
        <h4> Update Customer</h4>
-       <TextField type="number" name="CustomerID" variant="filled"
+       <TextField type="number" name="CustomerID" variant="filled" min="0"
         color="primary"   label="CustomerID"
         style={{ margin: 10 ,display: 'inline-block' }}  required></TextField>
-             <TextField type="text" name="username" onChange={this.onChange.bind(this)} value={this.state.value} id="regusername" variant="filled"
+             <TextField type="text" name="username" pattern="[A-Za-z]"  onChange={this.onChange.bind(this)} value={this.state.value} id="regusername" variant="filled"
         color="primary"   label="username"
         style={{ margin: 10 ,display: 'inline-block' }}  required></TextField>
            
@@ -690,11 +690,11 @@ class User extends React.Component {
         color="primary"   label="email"
         style={{ margin: 10 ,display: 'inline-block' }}  required></TextField>
            
-              <TextField type="text" name="phone"  id="regphone" variant="filled"
+              <TextField type="text" name="phone"  id="regphone" variant="filled" min="4000000000" max="4999999999"
         color="primary"   label="phone"
         style={{ margin: 10 ,display: 'inline-block' }}  required></TextField>
     
-              <TextField type="number" name="postcode"  id="regpostcode"  variant="filled"
+              <TextField type="number" name="postcode"  id="regpostcode"  variant="filled" min="0" max="9999"
         color="primary"   label="postcode"
         style={{ margin: 10 ,display: 'inline-block' }}  required></TextField>
           
